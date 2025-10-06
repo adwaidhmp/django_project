9f338d139b56cea2a0a0eede252382b3f5179078
Adwaidh Mp <adwaidh7727@gmail.com>
2025-10-06 09:19:46 +0530

    Update settings.py

--- Student/settings.py
+++ Student/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'student_django_project',
-        'USER': 'admin',
-        'PASSWORD': '12345',
+        'USER': 'youradmin',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -142,6 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'          
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adwaith7727@gmail.com'
-EMAIL_HOST_PASSWORD = 'sevbjnmllzecydig'   
+EMAIL_HOST_USER = 'your@mail'
+EMAIL_HOST_PASSWORD = 'Yourpassword'   
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
